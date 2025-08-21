ac1f4f4b05242e86b9d2322ea4c05836282a8f01
0801eflu-web <0801eflu@skola.falkoping.se>
2025-08-21 09:40:48 +0000

    initial project setup with .NET 8.0 framework and basic console application structure

---
+++ Program.cs
@@ -0,0 +1,118 @@
+﻿// Console.WriteLine("Välkommen Ben. Skriv ett tal");
+
+// string im1 = Console.ReadLine();
+// Console.WriteLine("ett till tal");
+// string inm2 = Console.ReadLine();
+
+// int tal1 = Convert.ToInt32(im1);
+
+// int tal2 = Convert.ToInt32(inm2);
+
+// int summa = tal1 + tal2;
+
+// Console.WriteLine("" + summa);
+
+// Console.WriteLine("Välkommen Ost. Skriv ett tal");
+
+// string inm1 = Console.ReadLine();
+// Console.WriteLine("ett til tal");
+// string inm2 = Console.ReadLine();
+
+// int tal1 = Convert.ToInt32(inm1);
+
+// int tal2 = Convert.ToInt32(inm2);
+// int summa = tal1 - tal2;
+// Console.WriteLine("" + summa);
+
+// Console.WriteLine("Välkommen Hank. Skriv ett tal");
+
+// string inm1 = Console.ReadLine();
+// Console.WriteLine("skriv ett till tal");
+// string inm2 = Console.ReadLine();
+
+// int tal1 = Convert.ToInt32(inm1);
+// int tal2 = Convert.ToInt32(inm2);
+// int summa = tal1 / tal2;
+// Console.WriteLine("" + summa);
+
+// Console.WriteLine("Välkommen Baam, skriv ett tal");
+
+// String inm1 = Console.ReadLine();
+// Console.WriteLine("skriv ett till tal");
+// string inm2 = Console.ReadLine();
+// int tal1 = Convert.ToInt32(inm1);
+// int tal2 = Convert.ToInt32(inm2);
+// int summa = tal1 * tal2;
+// Console.WriteLine("" + summa);
+
+// Console.WriteLine("Välkommen Baam, skriv ett tal");
+
+// String inm1 = Console.ReadLine();
+// Console.WriteLine("skriv ett till tal");
+// string inm2 = Console.ReadLine();
+
+// double tal1 = Convert.ToDouble(inm1);
+// double tal2 = Convert.ToDouble(inm2);
+
+// double div = tal1 / tal2;
+// Console.WriteLine("" + div);
+
+// Console.WriteLine("Välkommen Baam, skriv ett tal");
+// string inm1 = Console.ReadLine();
+// Console.WriteLine("skriv ett till tal");
+// string inm2 = Console.ReadLine();
+// int tal1 = Convert.ToInt32(inm1);
+// int tal2 = Convert.ToInt32(inm2);
+// int summa = tal1 + tal2;
+// Console.WriteLine("" + summa);
+
+Console.WriteLine("Välkommen Baam. skriv ett tal");
+string inm1 = Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Console.WriteLine("Välkommen!\nVad heter du?");
+
+// // string namn = Console.ReadLine();
+
+// // Console.WriteLine("Hej " + namn);
+// // Console.WriteLine("Vart bor du?");
+// // String plats = Console.ReadLine();
+
+// // Console.WriteLine("Vad gillar du att se på netflix?");
+// // String netflix = Console.ReadLine();
+
+// // Console.WriteLine($"Välkommen {namn}. Jag ser att du bor i/på {plats}.\n"
+// // + $"Ikväll tycker jag du ska kolla på {netflix}"); 
+
+
+// // Console.WriteLine("Hur gammal är du på riktigt");
+
+// // string inm = Console.ReadLine();
+// // int age = Convert.ToInt32(inm);
+
+// // if (age > 17)
+// // {
+// //     Console.WriteLine("Välkommen till krogen");
+// // }
+// // Console.WriteLine("skriv in ett heltal");
+// // string inm1 = Console.ReadLine();
+// // Console.WriteLine("skriv in ett till heltal");
+// // string inm2 = Console.ReadLine();
+
+// // int tal1 = Convert.ToInt32(inm1);
+// // int tal2 = Convert.ToInt32(inm2);
+
+// // int summa = tal1 + tal2;
+
+// // Console.WriteLine($"{tal1} + {tal2} = {summa}");
